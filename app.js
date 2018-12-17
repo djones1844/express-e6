6aeb0a393d0a6604f7203058389247ca41343040
Daniel Jones <djones1844@gmail.com>
2018-12-17 12:02:15 -0800

    added demo noSQL crud

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const logger = require('morgan')
 
 const indexRouter = require('./routes/index')
 const usersRouter = require('./routes/users')
-
+const nosqlRouter = require('./routes/demo1')
 const app = express()
 
 // view engine setup
@@ -22,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', indexRouter)
 app.use('/users', usersRouter)
+app.use('/demo1', nosqlRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
